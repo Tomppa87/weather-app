b175283779c0ad0e27e24e2f5818b2f820e675c1
Thomas Byrne <byrnesy987@gmail.com>
2025-04-18 18:20:48 +0300

    added current date

--- src/index.js
+++ src/index.js
@@ -1,39 +1,47 @@
 import "./styles.css";
-const body = document.querySelector("body")
+const body = document.querySelector("body");
 function farenheitToCelcius(x) {
-    let num = ((x-32)*5/9)
-    return num.toFixed()
+  let num = ((x - 32) * 5) / 9;
+  return num.toFixed();
 }
 
-const url = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/"
-const key = "?key=XPC5LAZK45WC8V2M2GJRUV8SG"
-const currentLocation = []
+const url =
+  "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/";
+const key = "?key=XPC5LAZK45WC8V2M2GJRUV8SG";
+const currentLocation = [];
 class Location {
-    constructor(name, temperature, conditions) {
-        this.name = name;
-        this.temperature = farenheitToCelcius(temperature);
-        this.conditions = conditions;
-        
-    } get celsius() {
-        return (farenheitToCelcius(this.temperature))
-    }
+  constructor(name, temperature, conditions) {
+    this.name = name;
+    this.temperature = farenheitToCelcius(temperature);
+    this.conditions = conditions;
+  }
+  get celsius() {
+    return farenheitToCelcius(this.temperature);
+  }
 }
 function newLocation(LocationName, locationTemperature, locationConditions) {
-    let location = new Location(LocationName, locationTemperature, locationConditions);
-    currentLocation.splice(0,1,location)
+  let location = new Location(
+    LocationName,
+    locationTemperature,
+    locationConditions,
+  );
+  currentLocation.splice(0, 1, location);
 }
 
 async function getCityData(city) {
-    try {
-        const response = await fetch(url+city+key, {mode: "cors"})
-        const responseData = await response.json();
-        console.log(responseData)
-        newLocation(responseData.address, responseData.currentConditions.temp, responseData.currentConditions.conditions)
-        updateDOM()
-    } catch (error) {
-        console.error();
-        
-    }
+  try {
+    const response = await fetch(url + city + key, { mode: "cors" });
+    const responseData = await response.json();
+    console.log(responseData);
+    newLocation(
+      responseData.address,
+      responseData.currentConditions.temp,
+      responseData.currentConditions.conditions,
+    );
+    updateDOM();
+  } catch (error) {
+    console.error();
+  }
 }
 
 // create card for weather
@@ -41,21 +49,26 @@ const content = document.getElementById("content");
 const weatherCard = document.createElement("div");
 weatherCard.classList.add("weatherCard");
 function updateDOM() {
-    while (weatherCard.firstChild) {
-        weatherCard.removeChild(weatherCard.firstChild)
-    }
-    for (const property in currentLocation[0]) {
-        const weatherProperty = document.createElement("div");
-        weatherProperty.id = property
-        weatherProperty.innerHTML = currentLocation[0][property];
-        weatherCard.appendChild(weatherProperty);
-        content.appendChild(weatherCard)
-    }
+  while (weatherCard.firstChild) {
+    weatherCard.removeChild(weatherCard.firstChild);
+  }
+  let date = new Date();
+  const dateDiv = document.createElement("div");
+  dateDiv.innerHTML = date;
+  weatherCard.appendChild(dateDiv);
+  console.log(date);
+  for (const property in currentLocation[0]) {
+    const weatherProperty = document.createElement("div");
+    weatherProperty.id = property;
+    weatherProperty.innerHTML = currentLocation[0][property];
+    weatherCard.appendChild(weatherProperty);
+    content.appendChild(weatherCard);
+  }
 }
-const searchBar = document.querySelector("input")
-const searchBtn = document.querySelector("button")
+const searchBar = document.querySelector("input");
+const searchBtn = document.querySelector("button");
 
 searchBtn.addEventListener("click", () => {
-    const searchInput = searchBar.value;
-    getCityData(searchInput);
-})
+  const searchInput = searchBar.value;
+  getCityData(searchInput);
+});
